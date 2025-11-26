8d2c700eb2c0db8763189d398ef8169fd50cbbfb
pd <pd@iaaspd02.png.intel.com>
2025-11-26 17:55:57 +0800

    fix: userConsent req to return more specific http sts codes
    Signed-off-by: pd <pd@iaaspd02.png.intel.com>

--- src/routes/amt/userConsent/request.ts
+++ src/routes/amt/userConsent/request.ts
@@ -8,6 +8,7 @@ import { logger, messages } from '../../../logging/index.js'
 import { ErrorResponse } from '../../../utils/amtHelper.js'
 import { MqttProvider } from '../../../utils/MqttProvider.js'
 import { AMTStatusCodes } from '../../../utils/constants.js'
+import { mapAMTReturnValueToHttpStatus, getDetailedErrorMessage } from './statusMapper.js'
 
 export async function request(req: Request, res: Response): Promise<void> {
   try {
@@ -20,13 +21,23 @@ export async function request(req: Request, res: Response): Promise<void> {
         Body: response.Body.StartOptIn_OUTPUT
       }
       result.Body.ReturnValueStr = AMTStatusCodes[result.Body.ReturnValue]
+      
       if (result.Body.ReturnValue.toString() === '0') {
         MqttProvider.publishEvent('success', ['Request_User_Consent_Code'], messages.USER_CONSENT_REQUEST_SUCCESS, guid)
         res.status(200).json(result)
       } else {
-        logger.error(`${messages.USER_CONSENT_REQUEST_FAILED} for guid : ${guid}.`)
+        // Map AMT return value to appropriate HTTP status code
+        const httpStatus = mapAMTReturnValueToHttpStatus(result.Body.ReturnValue, 'request')
+        const errorDetail = getDetailedErrorMessage(result.Body.ReturnValue, result.Body.ReturnValueStr, 'request')
+        
+        logger.error(`${messages.USER_CONSENT_REQUEST_FAILED} for guid : ${guid}. ReturnValue: ${result.Body.ReturnValue} (${result.Body.ReturnValueStr})`)
         MqttProvider.publishEvent('fail', ['Request_User_Consent_Code'], messages.USER_CONSENT_REQUEST_FAILED, guid)
-        res.status(400).json(result)
+        
+        // Return detailed error response with original AMT data
+        res.status(httpStatus).json({
+          ...errorDetail,
+          amtResponse: result
+        })
       }
     } else {
       logger.error(`${messages.USER_CONSENT_REQUEST_FAILED} for guid : ${guid}.`)
